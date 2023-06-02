5175fa34670ed507c5695ef52d65482ea14015f4
ARCHXTEKK <slaughterer_archxtekk@mail.ru>
2023-06-02 12:24:49 +0500

    finally

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import './index.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div> </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  render() {
+    return (
+      <div className='counter__outer'>
+        <div className='counter'>
+          {this.state.count}
+        </div>
+        <div className='btn__outer'>
+          <button className='btn decrement' onClick={this.decrement}>-</button>
+          <button className='btn increment' onClick={this.increment}>+</button>
+        </div>
+      </div>
+    );
+  }
+  increment() {
+    console.log("increment");
+    this.setState({count: this.state.count + 1});
+  }
+  decrement() {
+    this.setState({count: this.state.count - 1});
+  }
 }
 
 export default App;
